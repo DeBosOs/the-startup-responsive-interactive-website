b7352e0fda516b9d28eb9977479ba44163e45332
DeBosOs <luc.hoogerwerf@hva.nl>
2025-01-26 16:37:15 +0100

    interactie knop #16

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
-// koop animatie
+// purchase animation
 
-const koopKnop = document.querySelector("#cart");
+const koopKnop = document.querySelector(".cart");
 const mandje = document.querySelector("#cartIcon");
 
 koopKnop.addEventListener('click', () => {
     mandje.classList.toggle('bewegen');
-    
-    // Change the image source
     if (mandje.src.includes("mandje.png")) {
         mandje.src = "images/mandje2.png";
     } else {
