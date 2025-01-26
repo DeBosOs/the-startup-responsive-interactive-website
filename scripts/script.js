a4163f01501c89aea8f513030b40ffcaaf5cdae7
DeBosOs <luc.hoogerwerf@hva.nl>
2025-01-26 14:04:37 +0100

    interactie na feedback #14

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,14 @@
 const koopKnop = document.querySelector("#cart");
 const mandje = document.querySelector("#cartIcon");
 
-var aantal = 0;
-
 koopKnop.addEventListener('click', () => {
     mandje.classList.toggle('bewegen');
+    
+    // Change the image source
+    if (mandje.src.includes("mandje.png")) {
+        mandje.src = "images/mandje2.png";
+    } else {
+        mandje.src = "images/mandje.png";
+    }
 });
+
